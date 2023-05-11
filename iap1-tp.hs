04ad4b036f60465d163dd63576705e8df9c7eda8
SrRenho <juanjogvizioli@gmail.com>
2023-05-10 23:20:10 -0300

    Update iap1-tp.hs
    Datos del grupo
    Predicados 1,16,17,18,19,20,21

--- iap1-tp.hs
+++ iap1-tp.hs
@@ -1,10 +1,8 @@
--- Completar con los datos del grupo
---
--- Nombre de Grupo: xx
--- Integrante 1: Nombre Apellido, email, LU
--- Integrante 2: Nombre Apellido, email, LU
--- Integrante 3: Nombre Apellido, email, LU
--- Integrante 4: Nombre Apellido, email, LU
+-- Nombre de Grupo: Empanada4Quesos
+-- Integrante 1: Micaela Valeria Garrone, micaelagarrone@gmail.com, 860/23
+-- Integrante 2: Valeria Andreina Simoza Sanchez, vsimoza.vs@gmail.com, 1027/22
+-- Integrante 3: Lucia Berterreix, berterreixlucia@gmail.com, 1204/22
+-- Integrante 4: Juan José García Vizioli, juanjogvizioli@gmail.com, 353/20
 
 type Usuario = (Integer, String) -- (id, nombre)
 type Relacion = (Usuario, Usuario) -- usuarios que se relacionan
@@ -74,3 +72,38 @@ tieneUnSeguidorFiel = undefined
 -- describir qué hace la función: .....
 existeSecuenciaDeAmigos :: RedSocial -> Usuario -> Usuario -> Bool
 existeSecuenciaDeAmigos = undefined
+
+pertenece :: (Eq t) => t -> [t] -> Bool
+pertenece _ [] = False
+pertenece x (y:ys)  | (x == y) || (pertenece x ys) = True
+                    | otherwise = False
+
+cadenaDeAmigos :: [Usuario] -> RedSocial -> Bool
+cadenaDeAmigos [] r = True
+cadenaDeAmigos [x] r = True
+cadenaDeAmigos (x:y:ys) r   | relacionadosDirecto x y r && cadenaDeAmigos (y:ys) r = True
+                            | otherwise = False
+
+relacionadosDirecto :: Usuario -> Usuario -> RedSocial -> Bool
+relacionadosDirecto u1 u2 r = (pertenece (u1,u2) (relaciones r)) || (pertenece (u2,u1) (relaciones r)) 
+
+sonDeLaRed :: RedSocial ->  [Usuario] -> Bool
+sonDeLaRed r [] = True
+sonDeLaRed r (u:us) | pertenece u (usuarios r) && sonDeLaRed r us = True
+                    | otherwise = False
+
+empiezaCon :: (Eq t) => t -> [t] -> Bool
+empiezaCon x [] = False
+empiezaCon x y  | x == head y = True
+                | otherwise = False
+
+terminaCon :: (Eq t) => t -> [t] -> Bool
+terminaCon x [] = False
+terminaCon x [y]    | x == y = True
+                    | otherwise = False
+terminaCon x (y:ys)  = terminaCon x ys
+
+sinRepetidos :: (Eq t) => [t] -> Bool
+sinRepetidos [x] = True
+sinRepetidos (x:xs) | pertenece x xs = False
+                    | otherwise = sinRepetidos xs
